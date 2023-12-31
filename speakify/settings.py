721e39cd64f65ed5cf7878c0e19135e42fb875c8
Gagam <gaganjeetwork99@gmail.com>
2023-12-31 17:01:20 +0530

    added channel name

--- speakify/settings.py
+++ speakify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@^1kiev2=2viag4$033fpbjn_nfegi3=^b1pq#hsvzpi^m(n6&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh' '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
