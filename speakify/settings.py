bffc2335d8f990e47d6aabe120e26e64b3bc9c64
Gagam <gaganjeetwork99@gmail.com>
2023-12-31 18:02:13 +0530

    added channel name

--- speakify/settings.py
+++ speakify/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@^1kiev2=2viag4$033fpbjn_nfegi3=^b1pq#hsvzpi^m(n6&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh' '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
