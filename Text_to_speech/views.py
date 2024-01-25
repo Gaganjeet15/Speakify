7bec1955fe4e1c560b5ed48317a9c8ca0dc39352
Gaganjeet15 <gaganjeetwork99@gmail.com>
2024-01-25 22:19:39 +0530

    removed play button and added the sample audio for all the voices

--- Text_to_speech/views.py
+++ Text_to_speech/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import elevenlabs
 from django.http import JsonResponse
 from PyPDF2 import PdfReader
+elevenlabs.set_api_key('6f74a30fefe9236f5b558cba1c98811e')
 
 
 # Create your views here.
@@ -13,53 +14,53 @@ def speakify(request):
     return render(request, 'code.html', {})
 
 # Handle the form submission for generating and playing audio
+
+
 def generate_and_play_audio(request):
     if request.method == 'POST':
         voice_input = request.POST.get('voice_input')
         text_input = request.POST.get('text_input')
-        action = request.POST.get('action')
 
         voice_mapping = {
-             "Adam": elevenlabs.generate,
-             "Antoni": elevenlabs.generate,
-             "Arnold": elevenlabs.generate,
-             "Callum": elevenlabs.generate,
-             "Charlie": elevenlabs.generate,
-             "Charlotte": elevenlabs.generate,
-             "Clyde": elevenlabs.generate,
-             "Daniel": elevenlabs.generate,
-             "Dave": elevenlabs.generate,
-             "Domi": elevenlabs.generate,
-             "Dorothy": elevenlabs.generate,
-             "Ethan": elevenlabs.generate,
-             "Freya": elevenlabs.generate,
-              "Gigi": elevenlabs.generate,
-              "Giovanni": elevenlabs.generate,
-              "Glinda": elevenlabs.generate,
-              "Grace": elevenlabs.generate,
-              "Harry": elevenlabs.generate,
-              "James": elevenlabs.generate,
-              "Jeremy": elevenlabs.generate,
-              "Joseph": elevenlabs.generate,
-              "Josh": elevenlabs.generate,
-              "Liam": elevenlabs.generate,
-              "Matilda": elevenlabs.generate,
-              "Matthew": elevenlabs.generate,
-              "Michael": elevenlabs.generate,
-              "Mimi": elevenlabs.generate,
-              "Nicole": elevenlabs.generate,
-              "Patrick": elevenlabs.generate,
-              "Rachel": elevenlabs.generate,
-              "Sam": elevenlabs.generate,
-              "Serena": elevenlabs.generate,
-              "Thomas": elevenlabs.generate,  
+            "Adam": elevenlabs.generate,
+            "Antoni": elevenlabs.generate,
+            "Arnold": elevenlabs.generate,
+            "Callum": elevenlabs.generate,
+            "Charlie": elevenlabs.generate,
+            "Charlotte": elevenlabs.generate,
+            "Clyde": elevenlabs.generate,
+            "Daniel": elevenlabs.generate,
+            "Dave": elevenlabs.generate,
+            "Domi": elevenlabs.generate,
+            "Dorothy": elevenlabs.generate,
+            "Ethan": elevenlabs.generate,
+            "Freya": elevenlabs.generate,
+            "Gigi": elevenlabs.generate,
+            "Giovanni": elevenlabs.generate,
+            "Glinda": elevenlabs.generate,
+            "Grace": elevenlabs.generate,
+            "Harry": elevenlabs.generate,
+            "James": elevenlabs.generate,
+            "Jeremy": elevenlabs.generate,
+            "Joseph": elevenlabs.generate,
+            "Josh": elevenlabs.generate,
+            "Liam": elevenlabs.generate,
+            "Matilda": elevenlabs.generate,
+            "Matthew": elevenlabs.generate,
+            "Michael": elevenlabs.generate,
+            "Mimi": elevenlabs.generate,
+            "Nicole": elevenlabs.generate,
+            "Patrick": elevenlabs.generate,
+            "Rachel": elevenlabs.generate,
+            "Sam": elevenlabs.generate,
+            "Serena": elevenlabs.generate,
+            "Thomas": elevenlabs.generate,
         }
 
         if voice_input not in voice_mapping:
             return HttpResponse(f"Voice '{voice_input}' not recognized.")
 
         generate_function = voice_mapping[voice_input]
-        audio = generate_function(text=text_input, voice=voice_input, model="eleven_multilingual_v2")
 
         if not generate_function:
             return HttpResponse(f"Error: Unable to find generate function for voice '{voice_input}'.")
@@ -75,17 +76,13 @@ def generate_and_play_audio(request):
         if not audio.startswith(b'\xFF\xFB'):
             return HttpResponse("Error: Invalid audio format. Expected MP3 data.")
 
-        if action == 'play':
-            elevenlabs.play(audio)
-            return HttpResponse("Audio played successfully.")
-        elif action == 'download':
-            response = HttpResponse(audio, content_type='audio/mpeg')
-            response['Content-Disposition'] = 'attachment; filename="output.mp3"'
-            return response
-        else:
-            return HttpResponse("Error: Invalid action.")
+        response = HttpResponse(audio, content_type='audio/mpeg')
+        response['Content-Disposition'] = 'attachment; filename="output.mp3"'
+        return response
+
     else:
         return render(request, 'Text_to_speech/generate_audio.html')
+
 # Extract text from a PDF file
 def extract_text(request):
     if request.method == 'POST' and request.FILES.get('pdfFile'):
--- speakify/settings.py
+++ speakify/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@^1kiev2=2viag4$033fpbjn_nfegi3=^b1pq#hsvzpi^m(n6&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
